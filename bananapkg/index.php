1b7d8dabd8e71f46d0ec85d657b05c2a41ecf540
slackjeff <lrcjefferson@gmail.com>
2019-02-02 16:04:12 -0200

    Add files via upload

---
+++ bananapkg/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>BananaPKG - Simple Package Manager</title>
+	<meta charset="utf-8">
+	<style>
+		body{font-size: 1.2em; color: #232323;font-family: courier new; margin: 8%;}
+		.logo{width: 18%;}
+		hr{border: 1px dashed; color: #333; margin-top: 4%; margin-bottom: 4%;}
+		h3{margin-top: 3%; margin-bottom: 3%;}
+		#principal{margin-bottom: 12%;}
+		li#categoria{margin: 1.5%;}
+	</style>
+</head>
+<body>
+	<nav>
+		<img class='logo' src="bananapkg.png">
+	</nav>
+
+<ul>
+	<li><a href="doc-ptbr.html">Documentação em PT-BR</a></li>
+	<li><a href="#">Documentation in English</a></li>
+</ul>
+
+<h2>Deps</h2>
+<ul>
+	<li>Bash >= 4.4.18</li>
+	<li>GNU Sed >= 4.2.2</li>
+	<li>GNU Tar >= 1.30</li>
+	<li>xz >= 5.2.2</li>
+</ul>
+
+<hr>
